90fa713789d410a9ae4d675aa47917dd11e2bbda
Puru <puruchoudhary@gmail.com>
2010-11-17 22:22:16 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Listkick::Application.config.secret_token = 'be20dabd7a410b3882881389e507eca640dfaf9fe96c3ed54f25cf4d4934de6566f2f0be2fac2ecffdf93f9fb17295045949d3ebb487cbe5e6607aab3dafa875'
